7b5ed12510c8dab5019efac7ec526a2bd84cd1ce
Igor Muhachev <muhachev@yandex.ru>
2015-03-05 22:13:23 +0300

    updated recipe04.js accordingly with v2.0

--- chapter01/recipe04.js
+++ chapter01/recipe04.js
@@ -4,12 +4,16 @@
  * Chapter 1 | Getting Started with PhantomJS
  * Recipe 4  | Running a PhantomJS script with arguments
  */
-if (phantom.args.length === 0) {
-  console.log('No arguments were passed in.');
-} else {
-  phantom.args.forEach(function(arg, index) {
-    console.log('[' + index + '] ' + arg);
-  });
-}
+//if (phantom.args.length === 0) {
+//  console.log('No arguments were passed in.');
+//} else {
+//  phantom.args.forEach(function(arg, index) {
+//    console.log('[' + index + '] ' + arg);
+//  });
+//}
+
+require('system').args.forEach(function(arg){
+  console.log(arg);
+});
 
 phantom.exit();
